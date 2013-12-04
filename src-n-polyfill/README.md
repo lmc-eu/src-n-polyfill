4442b4f902e7a932813ff48aa1ea9e31faa813fb
vondrav <vojtech.vondra@lmc.eu>
2013-12-04 10:52:54 +0100

    Update docs to new version

--- src-n-polyfill/README.md
+++ src-n-polyfill/README.md
@@ -3,11 +3,25 @@ src-n-polyfill
 
 Javascript SRC-N proposal for responsive images polyfill
 
+Only 2,3KB minified and gzipped.
+
 Based on [Proposal for RespImg Syntax](http://tabatkins.github.io/specs/respimg/Overview.html).
+
+Browser support
+---------------
+
+Works out of the box for any modern browser supporting [window.matchMedia](http://caniuse.com/matchmedia) and [ES5 array functions](http://kangax.github.io/es5-compat-table/#Array.prototype.forEach).
+
+If you need IE 8 or IE 9 support, use the corresponding polyfills.
+
 
 Changelog
 ---------
 
-v0.3.0  jQuery independent
-v0.2.0  Reorganize and edit bower dependencies
-v0.1.0  Initial version
+- v0.4.0  Rewritten scope and dropped support for IE <= 9
+- v0.3.0  jQuery independent
+- v0.2.0  Reorganize and edit bower dependencies
+- v0.1.0  Initial version
+
+[![Bitdeli Badge](https://d2weczhvl823v0.cloudfront.net/lmc-eu/src-n-polyfill/trend.png)](https://bitdeli.com/free "Bitdeli Badge")
+
--- src-n-polyfill/src-n-polyfill.js
+++ src-n-polyfill/src-n-polyfill.js
@@ -1825,42 +1825,52 @@ module.exports = (function(){
 },{}],2:[function(require,module,exports){
 var ResponsiveImage = require('./responsiveImage.js');
 var parse = require("./../components/src-n-parse/index.js").parse;
-var images = [];
-
-function addToCollection(element) {
-  var i,
-      rules = [],
-      attrs = element.attributes;
-
-  for (i = 0; i < attrs.length; ++i) {
-    if (attrs[i].name.match(/^src(-[0-9]+)?$/i)){
-      rules.push(parse(attrs[i].value));
-    }
-  }
-  console.log(rules);
-  images.push(new ResponsiveImage(
-    element,
-    rules
-  ));
-}
-
-function buildCollection() {
-  var i,
-      imagesWithSrc1 = document.querySelectorAll('img[src-1]');
-    
-  for (i = 0; i < imagesWithSrc1.length; ++i) {
-    addToCollection(imagesWithSrc1[i]);
-  }
-}
-
-function renderAll() {
-  images.forEach(function(img){
-    img.setImage();
-  });
-}
 
 window.srcnpolyfill = function() {
-  buildCollection();
+  var images = [];
+
+  function createResponsiveImage(element) {
+    var i,
+        rules = [],
+        attrs = element.attributes;
+
+    for (i = 0; i < attrs.length; ++i) {
+      if (attrs[i].name.match(/^src(-[0-9]+)?$/i)) {
+        try {
+          rules.push(parse(attrs[i].value));
+        } catch (e) {
+          /* Invalid src-n attribute, silently ingore like any other HTML you don't know */
+        }
+      }
+    }
+
+    return new ResponsiveImage(element, rules);
+  }
+
+  function buildCollection() {
+    var i,
+        imgs = [],
+        imagesWithSrc1 = document.querySelectorAll('img[src-1]');
+
+    for (i = 0; i < imagesWithSrc1.length; ++i) {
+      imgs.push(createResponsiveImage(imagesWithSrc1[i]));
+    }
+
+    return imgs;
+  }
+
+  function renderAll() {
+    images.forEach(function(img) {
+      img.setImage();
+    });
+  }
+
+  // Browsers not supporting ES5 are not responsive anyway
+  if (!Array.prototype.forEach || !window.matchMedia) {
+    return;
+  }
+
+  images = buildCollection();
   renderAll();
 
   if (Element.prototype.addEventListener) {
