c83b703500f4850528a27f9c219a54593cc0e126
vondrav <vojtech.vondra@lmc.eu>
2013-12-04 10:36:34 +0100

    Refactor helper functions into polyfill scope and omit wide scope usage

--- src/index.js
+++ src/index.js
@@ -1,47 +1,47 @@
 var ResponsiveImage = require('./responsiveImage.js');
 var parse = require('src-n-parse').parse;
-var images = [];
-
-function addToCollection(element) {
-  var i,
-      rules = [],
-      attrs = element.attributes;
-
-  for (i = 0; i < attrs.length; ++i) {
-    if (attrs[i].name.match(/^src(-[0-9]+)?$/i)){
-      rules.push(parse(attrs[i].value));
-    }
-  }
-  console.log(rules);
-  images.push(new ResponsiveImage(
-    element,
-    rules
-  ));
-}
-
-function buildCollection() {
-  var i,
-      imagesWithSrc1 = document.querySelectorAll('img[src-1]');
-    
-  for (i = 0; i < imagesWithSrc1.length; ++i) {
-    addToCollection(imagesWithSrc1[i]);
-  }
-}
-
-function renderAll() {
-  images.forEach(function(img){
-    img.setImage();
-  });
-}
 
 window.srcnpolyfill = function() {
+  var images = [];
+
+  function createResponsiveImage(element) {
+    var i,
+        rules = [],
+        attrs = element.attributes;
+
+    for (i = 0; i < attrs.length; ++i) {
+      if (attrs[i].name.match(/^src(-[0-9]+)?$/i)) {
+        rules.push(parse(attrs[i].value));
+      }
+    }
+
+    return new ResponsiveImage(element, rules);
+  }
+
+  function buildCollection() {
+    var i,
+        imgs = [],
+        imagesWithSrc1 = document.querySelectorAll('img[src-1]');
+
+    for (i = 0; i < imagesWithSrc1.length; ++i) {
+      imgs.push(createResponsiveImage(imagesWithSrc1[i]));
+    }
+
+    return imgs;
+  }
+
+  function renderAll() {
+    images.forEach(function(img) {
+      img.setImage();
+    });
+  }
 
   // Browsers not supporting ES5 are not responsive anyway
   if (!Array.prototype.forEach || !window.matchMedia) {
     return;
   }
 
-  buildCollection();
+  images = buildCollection();
   renderAll();
 
   if (Element.prototype.addEventListener) {
